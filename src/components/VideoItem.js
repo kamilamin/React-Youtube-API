80d03891410dbc0f7b55e032f4f2a9f83ea7729b
Kamil Amin <kamil.amin@mparsec.com>
2019-05-07 09:49:06 +0500

    --Commit: Video Item Functional Components is Developed and Styles file created

---
+++ src/components/VideoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/video.css";
+
+const VideoItem = ({ video, handleVideoSelect }) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default VideoItem;
