8d7136c59de12cb567bc6521b0411b12c78ca1e9
Kamil Amin <kamil.amin@mparsec.com>
2019-05-07 12:23:43 +0500

    --Commit: Videos Item Components for layout the page and display information about all the video

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,8 +3,18 @@ import "../style/video.css";
 
 const VideoItem = ({ video, handleVideoSelect }) => {
   return (
-    <div>
-      
+    <div
+      onClick={() => handleVideoSelect(video)}
+      className="video-item item"
+    >
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.description}
+      />
+      <div className="content">
+        <div className='header'>{video.snippet.title}</div>
+      </div>
     </div>
   );
 };
