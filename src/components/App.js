84362b32fe9fc2e373c5095b9444d519a03351ba
Kamil Amin <kamil.amin@mparsec.com>
2019-05-07 12:24:48 +0500

    --Commit: App.js Main Component for connecting all the components and functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import SearchBar from "./Searchbar";
+import youtube from "../apis/youtube-api";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetails";
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>App</h1>
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
+  handleSubmit = async termFromSearchBar => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: termFromSearchBar
+      }
+    });
+    this.setState({
+      videos: response.data.items
+    });
+  };
+  handleVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "1em" }}>
+        <SearchBar handleFormSubmit={this.handleSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
             </div>
-        );
-    }
+            <div className="five wide column">
+              <VideoList
+                handleVideoSelect={this.handleVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
